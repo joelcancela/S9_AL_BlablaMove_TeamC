547e1d6b108a6221941bf92ac730c34ec1a57104
Nassim Bounouas <nassim.bounouas@etu.unice.fr>
2018-10-16 23:29:21 +0200

    Spring minimal structure

---
+++ blablamovebackend/src/main/java/fr/polytech/unice/blablamove/teamc/blablamovebackend/BlablamovebackendApplication.java
@@ -0,0 +1,12 @@
+package fr.polytech.unice.blablamove.teamc.blablamovebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlablamovebackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlablamovebackendApplication.class, args);
+    }
+}
---
+++ blablamovebackend/src/test/java/fr/polytech/unice/blablamove/teamc/blablamovebackend/BlablamovebackendApplicationTests.java
@@ -0,0 +1,16 @@
+package fr.polytech.unice.blablamove.teamc.blablamovebackend;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BlablamovebackendApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
