5bf16fa41c1677fa0f2ea1301aa310ba638400dc
Nassim Bounouas <nassim.bounouas@etu.unice.fr>
2018-10-21 01:52:38 +0200

    Ngx-admin - Marketing marker

--- ngx-admin/src/app/pages/marketing/heatmap/heatmap.component.ts
+++ ngx-admin/src/app/pages/marketing/heatmap/heatmap.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import * as L from 'leaflet';
+import {tileLayer, latLng, Layer, marker, icon, Map} from 'leaflet';
 
 @Component({
   selector: 'ngx-heatmap',
@@ -8,17 +8,55 @@ import * as L from 'leaflet';
 })
 export class HeatmapComponent implements OnInit {
 
+  layer = tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 18, attribution: '...' });
+
   options = {
     layers: [
-      L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 18, attribution: '...' }),
+      this.layer,
     ],
     zoom: 10,
-    center: L.latLng({ lat: 43.700000, lng: 7.250000 }),
+    center: latLng({ lat: 43.700000, lng: 7.250000 }),
   };
+
+  markers: Layer[] = [];
+
+  markerw = marker(
+    [43.700000, 7.250000],
+    {
+      icon: icon({
+        iconSize: [ 25, 41 ],
+        iconAnchor: [ 13, 41 ],
+        iconUrl: 'assets/marker/marker.png',
+      }),
+    },
+  );
+
+  zoomend(map: Map) {
+    const zoom = map.layer._tileZoom;
+    this.markerw.iconSize = [0.48 * zoom, 0.48 * zoom];
+  }
+
+  addMarker() {
+    const newMarker = marker(
+      [ 43.700000, 7.250000 ],
+      {
+        icon: icon({
+          iconAnchor: [ 43.700000, 7.250000 ],
+          iconUrl: 'assets/marker/marker.png',
+        }),
+      },
+    );
+    this.markers.push(newMarker);
+  }
+
+  removeMarker() {
+    this.markers.pop();
+  }
 
   constructor() { }
 
   ngOnInit() {
+    this.markers.push(this.markerw);
   }
 
 }
