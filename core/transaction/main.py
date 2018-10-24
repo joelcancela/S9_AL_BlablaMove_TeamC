97d91bd919f740fab2f7962261b21a3a7f137728
tinvernizzi <tanguy.invernizzi@yahoo.fr>
2018-10-24 15:01:20 +0200

    Added simple Flask rest api

--- core/transaction/main.py
+++ core/transaction/main.py
@@ -1,1 +1,22 @@
-from kafka import KafkaProducer
+from flask import Flask
+from flask_restful import reqparse, abort, Api, Resource
+
+app = Flask(__name__)
+api = Api(app)
+
+numberOfPostCalls = 0
+
+class Counter(Resource):
+    def get(self):
+        return numberOfPostCalls
+
+    def post(self):
+        global numberOfPostCalls
+        numberOfPostCalls = numberOfPostCalls + 1
+        return "OK"
+
+
+api.add_resource(Counter, '/')
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8080)
