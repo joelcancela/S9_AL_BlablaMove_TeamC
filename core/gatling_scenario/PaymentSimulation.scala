827bb01c0cedbb597eccd50824accbc9e1f5462d
tinvernizzi <tanguy.invernizzi@yahoo.fr>
2018-10-26 10:45:54 +0200

    More complex gatling scenario

--- core/gatling_scenario/PaymentSimulation.scala
+++ core/gatling_scenario/PaymentSimulation.scala
@@ -15,6 +15,15 @@ class PaymentSimulation extends Simulation { // 3
     .pause(5) // 10
 
   setUp( // 11
-    scn.inject(atOnceUsers(1)) // 12
-  ).protocols(httpProtocol) // 13
+    scn.inject(
+	    nothingFor(4 seconds), // 1
+	    atOnceUsers(10), // 2
+	    rampUsers(10) during (5 seconds), // 3
+	    constantUsersPerSec(200) during (15 seconds), // 4
+	    constantUsersPerSec(200) during (15 seconds) randomized, // 5
+	    rampUsersPerSec(10) to 200 during (10 seconds), // 6
+	    rampUsersPerSec(10) to 200 during (10 seconds) randomized, // 7
+	    heavisideUsers(1000) during (20 seconds) // 8‡
+    ) // 12
+  )	.protocols(httpProtocol) // 13
 }
