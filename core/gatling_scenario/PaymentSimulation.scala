88273f205b9d5c77f5552216142eef606d899632
tinvernizzi <tanguy.invernizzi@yahoo.fr>
2018-11-07 08:40:31 +0100

    Basic login/logout/register scenario

--- core/gatling_scenario/PaymentSimulation.scala
+++ core/gatling_scenario/PaymentSimulation.scala
@@ -1,29 +1,38 @@
-package computerdatabase // 1
+package computerdatabase
 
-import io.gatling.core.Predef._ // 2
+import io.gatling.core.Predef._ 
 import io.gatling.http.Predef._
 import scala.concurrent.duration._
 
-class PaymentSimulation extends Simulation { // 3
+class PaymentSimulation extends Simulation { 
 
-  val httpProtocol = http // 4
-    .baseUrl("http://127.0.0.1:8080")
+  val userHttpProtocol = http 
+    .baseUrl("http://127.0.0.1:5002")
 
-  val scn = scenario("PaymentSimulation") // 7
-    .exec(http("request_1") // 8
-      .post("/")) // 9
-    .pause(5) // 10
+  val usersRegistering = scenario("UsersRegistering").exec(User.register)
+  val usersLoginAndLogout = scenario("UsersLoginAndLogout").exec(User.loginAndLogout)
 
-  setUp( // 11
-    scn.inject(
-	    nothingFor(4 seconds), // 1
-	    atOnceUsers(10), // 2
-	    rampUsers(10) during (5 seconds), // 3
-	    constantUsersPerSec(200) during (15 seconds), // 4
-	    constantUsersPerSec(200) during (15 seconds) randomized, // 5
-	    rampUsersPerSec(10) to 200 during (10 seconds), // 6
-	    rampUsersPerSec(10) to 200 during (10 seconds) randomized, // 7
-	    heavisideUsers(1000) during (20 seconds) // 8‡
-    ) // 12
-  )	.protocols(httpProtocol) // 13
+  setUp( 
+    usersRegistering.inject(
+      rampUsersPerSec(10) to 200 during (10 seconds) randomized,
+      heavisideUsers(1000) during (10 seconds)
+    ),
+    usersLoginAndLogout.inject(
+      rampUsersPerSec(10) to 200 during (10 seconds) randomized,
+    )
+  ).protocols(userHttpProtocol)
+}
+
+object User {
+  val register = exec(http("Register")
+      .post("/user/register")
+      )
+
+  val loginAndLogout = exec(http("Login")
+      .post("/user/login")
+      )
+    .pause(1, 5)
+    .exec(http("Logout")
+      .post("/user/logout")
+      )
 }
