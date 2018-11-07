c71831a574951d3e148514545b148eba594228a5
tinvernizzi <tanguy.invernizzi@yahoo.fr>
2018-11-07 10:50:36 +0100

    KPI Simulation

---
+++ core/gatling_scenario/BasicKPISimulation.scala
@@ -0,0 +1,21 @@
+package computerdatabase
+
+import io.gatling.core.Predef._ 
+import io.gatling.http.Predef._
+import scala.concurrent.duration._
+
+class PaymentSimulation extends Simulation { 
+
+  val userHttpProtocol = http 
+    .baseUrl("http://127.0.0.1:5001")
+
+  val askingForKPI = scenario("AskingForKPI").exec(http("ask")
+      .post("/kpi/city/top10")
+      )
+
+  setUp( 
+    usersRegistering.inject(
+      heavisideUsers(10) during (10 seconds)
+    )
+  ).protocols(userHttpProtocol)
+}
