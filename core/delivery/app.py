b0039b38fe1ec45bf213a3d7e986dacaab20b0e1
Nikita Rousseau <nikita.rousseau@etu.unice.fr>
2019-01-18 12:07:07 +0100

    Migrate delivery to Klein/twisted

--- core/delivery/app.py
+++ core/delivery/app.py
@@ -3,7 +3,6 @@
 import configparser
 import datetime
 import json
-import logging
 import os
 import queue
 import signal
@@ -11,13 +10,14 @@ import sys
 import threading
 import uuid
 from random import randint
+
+from klein import Klein
 from time import sleep
+from twisted.web.static import File
 
-from flask import Flask, jsonify, render_template
-from flask.logging import default_handler
-from kafka import KafkaProducer, KafkaConsumer
-
-from message.factory import make_kafka_message
+from producerconsumer.kafka_producer_worker import kafka_producer_worker
+from producerconsumer.kafka_consumer_worker import kafka_consumer_worker
+from message.make_kafka_message import make_kafka_message
 
 __product__ = "Core Delivery"
 __author__ = "Nikita ROUSSEAU"
@@ -29,9 +29,10 @@ __maintainer__ = "Nikita ROUSSEAU"
 __email__ = "nikita.rousseau@etu.unice.fr"
 __status__ = "development"
 
-# Flask Application
-app = Flask(__name__)
+# Web Application
+app = Klein()
 
+# GLOBAL SERVER SETTINGS
 # GLOBAL APPLICATION CONFIGURATION
 env = 'development'
 app_config = {}
@@ -42,6 +43,7 @@ threads = []
 threads_mq = {}
 # CLEAN EXIT EVENT
 t_stop_event = threading.Event()
+
 
 cities = {
     0: 'Marseille',
@@ -57,10 +59,13 @@ def __sigint_handler(signal, frame):
     Catch CTR+C / KILL signals
     Do housekeeping before leaving
     """
-    logging.debug("SIGINT or SIGTERM catched")
-    logging.debug("Raise t_stop_event")
+    print("SIGINT or SIGTERM catched")
+    print("Raise t_stop_event")
     t_stop_event.set()  # Set stop flag to true for all launched threads
-    logging.info("Stopping daemons...")
+    print("Stopping daemons...")
+    # Waiting threads...
+    for t in threads:
+        t.join()
     sleep(1)
     sys.exit(1)
 
@@ -73,8 +78,7 @@ def __load_config():
     """
     Parse database configuration file
     """
-    config_file = os.path.join(os.path.dirname(
-        os.path.realpath(__file__)), "config.ini")
+    config_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), "config.ini")
     if not os.path.exists(config_file):
         raise FileNotFoundError(config_file)
     app_config = configparser.ConfigParser()
@@ -87,17 +91,16 @@ def __load_config():
 ########################################################################################################################
 
 
-@app.route('/')
-def root_route():
+# We are serving a static file, branch=True is required
+@app.route('/', branch=True)
+def root_route(request):
     # API DOCUMENTATION ROOT
-    return render_template('swagger/index.html')
+    return File('./swagger')
 
 
 @app.route('/status')
-def status_route():
-    return jsonify(
-        {'status': 'online'}
-    ), 200
+def status_route(request):
+    return '200 OK'
 
 
 ########################################################################################################################
@@ -106,7 +109,7 @@ def status_route():
 
 @app.route("/delivery",
            methods=['POST'])
-def post_delivery_route():
+def post_delivery_route(request):
     """
     Create a new delivery
     :return:
@@ -125,12 +128,12 @@ def post_delivery_route():
     threads_mq['delivery'].put(message)
 
     # Response with callback url
-    return jsonify(message), 200
+    return json.dumps(dict(message),)
 
 
 @app.route("/delivery/checkpoint",
            methods=['POST'])
-def post_delivery_checkpoint_route():
+def post_delivery_checkpoint_route(request):
     """
     Notify that the delivery has reached a checkpoint
     :return:
@@ -153,12 +156,12 @@ def post_delivery_checkpoint_route():
     threads_mq['delivery'].put(message)
 
     # Response with callback url
-    return jsonify(message), 200
+    return json.dumps(dict(message),)
 
 
 @app.route("/delivery/issue",
            methods=['POST'])
-def post_delivery_issue():
+def post_delivery_issue(request):
     """
     Notify that a specific delivery has had an issue.
     :return:
@@ -175,7 +178,7 @@ def post_delivery_issue():
     threads_mq['delivery'].put(message)
 
     # Response with callback url
-    return jsonify(message), 200
+    return json.dumps(dict(message),)
 
 
 ########################################################################################################################
@@ -183,124 +186,23 @@ def post_delivery_issue():
 ########################################################################################################################
 
 
-def http_server_worker():
-    # Http server
-    app.run("0.0.0.0", port=5000, use_reloader=False, threaded=True)
-    return
-
-
-def kafka_hb_consumer_worker():
-    """
-    Kafka Heartbeat Broadcast Listener
-    :return:
-    """
-    # Client
-    consumer = KafkaConsumer('heartbeat',
-                             bootstrap_servers=bootstrap_servers,
-                             value_deserializer=lambda item: json.loads(item.decode('utf-8')))
-
-    while not t_stop_event.is_set():
-        try:
-            # Message loop
-            for message in consumer:
-                logging.info("READING MESSAGE %s:%d:%d: key=%s value=%s" % (
-                    message.topic,
-                    message.partition,
-                    message.offset,
-                    message.key,
-                    message.value)
-                )
-
-                # simple sanitizer
-                if 'action' not in message.value:
-                    logging.info("MALFORMED MESSAGE value=%s SKIPPING" %
-                                 (message.value,))
-                    continue
-
-                # Action switch
-                if str(message.value["action"]).upper() == 'HEARTBEAT_BROADCAST':
-                    logging.info("SENDING HEARTBEAT FOR " + __product__)
-                    # Send
-                    hb, request_id = make_kafka_message(
-                        action='HEARTBEAT_REPLY',
-                        message={
-                            'service_name': str(__product__),
-                            'timestamp': int(datetime.datetime.now().timestamp())
-                        }
-                    )
-                    threads_mq['heartbeat'].put(hb)
-        except Exception as e:
-            logging.fatal(e, exc_info=True)
-
-    consumer.close()
-    return
-
-def kafka_producer_worker(topic: str, mq: queue.Queue):
-    """
-    Kafka Generic Message Producer
-    as thread worker
-    Get messages from a shared mq queue.Queue
-    :param topic: str
-    :param mq: queue.Queue
-    :return:
-    """
-    # Client
-    producer = KafkaProducer(bootstrap_servers=bootstrap_servers,
-                             value_serializer=lambda item: json.dumps(item).encode('utf-8'))
-
-    while not t_stop_event.is_set():
-        # 10ms
-        sleep(0.01)
-        try:
-            if mq.qsize() > 0:
-                # Topic + Message
-                msg = mq.get()
-                logging.info("GET %s AND SENDING TO %s" % (msg, topic))
-                producer.send(topic, msg)
-                # Force buffer flush in order to send the message
-                logging.info("MESSAGE SENT !")
-                producer.flush()
-        except Exception as e:
-            logging.fatal(e, exc_info=True)
-
-    producer.close()
-    return
-
-
 if __name__ == '__main__':
     # ENVIRONMENT
     if len(sys.argv) > 1 and str(sys.argv[1]) == 'production':
         env = 'production'
-
-    if 'FLASK_ENV' not in os.environ:
-        os.environ['FLASK_ENV'] = env
-    if 'FLASK_APP' not in os.environ:
-        os.environ['FLASK_APP'] = __file__ + '.py'
-
-    if env == 'development':
-        os.environ['FLASK_DEBUG'] = '1'
     else:
-        os.environ['FLASK_DEBUG'] = '0'
-
-    # LOGGING
-    app.logger.removeHandler(default_handler)
-    if env == 'production':
-        logging.basicConfig(
-            level=logging.WARNING
-        )
-    else:
-        logging.basicConfig(
-            level=logging.INFO
-        )
+        env = 'development'
 
     # CONFIGURATION
     app_config_raw = __load_config()
     app_config = app_config_raw[env]
 
+    host = app_config['host']
+    port = app_config['port']
+
     # Bootstrap servers
     if ',' in str(app_config['bootstrap_servers']):
-        bootstrap_servers = list(
-            filter(None, str(app_config['bootstrap_servers']).split(',')))
+        bootstrap_servers = list(filter(None, str(app_config['bootstrap_servers']).split(',')))
     else:
         bootstrap_servers.append(str(app_config['bootstrap_servers']))
 
@@ -309,13 +211,6 @@ if __name__ == '__main__':
         topics = list(filter(None, str(app_config['topics']).split(',')))
     else:
         topics.append(str(app_config['topics']))
-
-    # WERKZEUG SERVER
-    t_http_server_worker = threading.Thread(
-        name='http_server_worker',
-        daemon=True,
-        target=http_server_worker
-    )
 
     ###########################################################
 
@@ -329,33 +224,32 @@ if __name__ == '__main__':
             name='kafka_' + topic + '_producer_worker',
             daemon=True,
             target=kafka_producer_worker,
-            args=(topic, mq,)
+            args=(t_stop_event, bootstrap_servers, topic, mq,)
         )
         threads.append(t_producer_worker)
 
     # heartbeat consumer
     t_kafka_hb_consumer_worker = threading.Thread(
-        name='kafka_hb_consumer_worker',
+        name='kafka_heartbeat_consumer_worker',
         daemon=True,
-        target=kafka_hb_consumer_worker
+        target=kafka_consumer_worker,
+        args=(t_stop_event, bootstrap_servers, 'heartbeat', __product__, threads_mq)
     )
     threads.append(t_kafka_hb_consumer_worker)
 
     ###########################################################
 
-    # Start
-    logging.warning(__product__ + ' version ' + __version__ +
-                    ' (' + env + ') is starting...')
-
     # Starting threads
     for t in threads:
         t.start()
-    t_http_server_worker.start()
 
-    # Waiting threads...
-    for t in threads:
-        t.join()
-    t_http_server_worker.join()
+    print(__product__ + ' version ' + __version__ + ' (' + env + ') is listening on socket "' + host + ':' + port + '"')
 
-    logging.info('Bye !')
+    # Http server
+    # No logs when set in production
+    log = open('app.log', 'a')
+    if env == 'production':
+        log = open('/dev/null', 'a')
+    app.run(host, port, log)
+
     exit(0)
