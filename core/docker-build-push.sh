0be6ac466ff83eaf7427337a3ef369d492a26568
Nikita Rousseau <nikita.rousseau@etu.unice.fr>
2019-02-04 14:12:12 +0100

    updated push script

--- core/docker-build-push.sh
+++ core/docker-build-push.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 cd ./kpi
 docker build -t blablamove/api-gateway/core/kpi .
-docker push blablamove/api-gateway/core/kpi:latest
+docker push blablamove/api-gateway/core/kpi
 cd ..
 
 cd ./delivery
 docker build -t blablamove/api-gateway/core/delivery .
-docker push blablamove/api-gateway/core/delivery:latest
+docker push blablamove/api-gateway/core/delivery
 cd ..
 
 cd ./user
 docker build -t blablamove/api-gateway/core/user .
-docker push blablamove/api-gateway/core/user:latest
+docker push blablamove/api-gateway/core/user
 cd ..
