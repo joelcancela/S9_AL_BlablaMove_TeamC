cdf4ee9199bdc8f98f99e56b042a7eda70ab81de
Nikita Rousseau <nikita.rousseau@etu.unice.fr>
2019-02-04 13:35:17 +0100

    Prefix route API

--- core/delivery/app.py
+++ core/delivery/app.py
@@ -86,13 +86,16 @@ def __load_config():
 
 
 # We are serving a static file, branch=True is required
-@app.route('/', branch=True)
+@app.route('/delivery/api',
+           methods=['GET'],
+           branch=True)
 def root_route(request):
     # API DOCUMENTATION ROOT
     return File('./swagger')
 
 
-@app.route('/status')
+# Health check
+@app.route('/delivery/healthz')
 def status_route(request):
     return '200 OK'
 
@@ -101,7 +104,7 @@ def status_route(request):
 # CORE DELIVERY SERVICE ROUTES
 ########################################################################################################################
 
-@app.route("/route",
+@app.route("/delivery/route",
            methods=['POST'])
 def post_route(request):
     """
