1e0823ab4390e397c6e1587794f4db9f1d3fa65a
Joël CANCELA VAZ <joel.cancelavaz@gmail.com>
2019-02-15 10:45:53 +0100

    Created routes composants

--- adminMarketingUI/src/app/pages/marketing/created-routes/created-routes.component.ts
+++ adminMarketingUI/src/app/pages/marketing/created-routes/created-routes.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {RouteCreated} from '../../../model/route-created';
+import {Subject} from 'rxjs';
+import {DashboardMarketingService} from '../../../services/dashboard-marketing.service';
 
 @Component({
   selector: 'created-routes',
   templateUrl: './created-routes.component.html',
   styleUrls: ['./created-routes.component.scss']
 })
-export class CreatedRoutesComponent implements OnInit {
+export class CreatedRoutesComponent implements OnInit, OnDestroy {
+  dtOptions: DataTables.Settings = {};
+  routesCreated: RouteCreated[] = [];
+  dtTrigger: Subject<any> = new Subject();
 
-  constructor() { }
+  constructor(private marketingService: DashboardMarketingService) {
+  }
 
   ngOnInit() {
+    this.dtOptions = {
+      pagingType: 'full_numbers',
+      pageLength: 2
+    };
+    this.marketingService.getLast24hCreatedRoutes()
+      .subscribe(routes => {
+        this.routesCreated = routes;
+        this.dtTrigger.next();
+      });
   }
+
+  ngOnDestroy(): void {
+    this.dtTrigger.unsubscribe();
+  }
+
 
 }
