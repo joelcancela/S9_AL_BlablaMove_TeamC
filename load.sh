ea3dc502c907850655cca102cda3da5b6c5e14cc
Joël CANCELA VAZ <joel.cancelavaz@gmail.com>
2019-02-28 10:07:59 +0100

    Load testing script

--- load.sh
+++ load.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
-#if [ $(uname -s) == "MINGW64_NT-10.0" ]
-#hen
-#MSYS_NO_PATHCONV=1 docker run -it --rm -v ${PWD}/loadTesting/conf:/opt/gatling/conf \
-#-v ${PWD}/loadTesting/user-files:/opt/gatling/user-files \
-#-v ${PWD}/loadTesting/results:/opt/gatling/results \
-#denvazh/gatling:2.3.1
-#else
-#docker run -it --rm -v ${PWD}/loadTesting/conf:/opt/gatling/conf \
-#-v ${PWD}/loadTesting/user-files:/opt/gatling/user-files \
-#-v ${PWD}/loadTesting/results:/opt/gatling/results \
-#denvazh/gatling:2.3.1
-#fi
+if [ $(uname -s) == "MINGW64_NT-10.0" ]
+then
+cd loadTesting
+MSYS_NO_PATHCONV=1 docker run -it --rm -v ${PWD}/loadTestingData/conf:/opt/gatling/conf \
+-v ${PWD}/loadTestingData/user-files:/opt/gatling/user-files \
+-v ${PWD}/loadTestingData/results:/opt/gatling/results \
+denvazh/gatling:2.3.1
+else
 cd loadTesting
 docker build -t loadtester .
 docker run -it --rm -v ${PWD}/loadTestingData/results:/opt/gatling/results loadtester
+fi
+cd ..
